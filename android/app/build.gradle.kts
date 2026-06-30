bf6d0ef2c1b85a0dcdbe26bf4b6f9cd11c8ea2f7
Rafael 'Ferreis' Mecabo <rafa_ferreis@hotmail.com>
2026-06-30 15:10:02 -0300

    feat(android): adicionar sufixo de ID de aplicação para debug e ajustar rótulo
    Adicionar sufixo ".dev" ao ID da aplicação na configuração de debug.
    Atualizar o rótulo da aplicação para usar o placeholder "appLabel" no AndroidManifest.xml.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,17 +15,20 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "br.com.ferreis.ordem_fichas"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        manifestPlaceholders["appLabel"] = "Fichas Ordem"
     }
 
     buildTypes {
+        debug {
+            applicationIdSuffix = ".dev"
+            manifestPlaceholders["appLabel"] = "Fichas Ordem Dev"
+        }
+
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
